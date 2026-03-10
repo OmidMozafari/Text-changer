05fc63b6b0013e11dabec1d6ad6ad4ae7bbe1cf6
OmidMozafari <omidmozafari444@gmail.com>
2026-03-10 18:03:16 +0430

    Third commit. At this time, worked on styling, layout and functionality of the project.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let textArea = document.getElementById("textArea")
-const UpperCasing = document.getElementById("UpperCasing")
-const LowerCasing = document.getElementById("LowerCasing")
-const Capitalizing = document.getElementById("Capitalizing")
-const deleteSpaces = document.getElementById("deleteSpaces")
-
+let textArea = document.getElementById("textArea");
+const UpperCasing = document.getElementById("UpperCasing");
+const LowerCasing = document.getElementById("LowerCasing");
+const Capitalizing = document.getElementById("Capitalizing");
+const deleteSpaces = document.getElementById("deleteSpaces");
+const wordCounter = document.getElementById("wordCounter");
+const charcterCounter = document.getElementById("charcterCounter");
+const reverseWords = document.getElementById("reverseWords");
 
 
 // To upper case funciton
 function upperCase(){
    textArea.value = textArea.value.toUpperCase()
-    return
+    return;
 }
 
 // to lower case function
@@ -17,25 +19,59 @@ function lowerCase(){
     textArea.value = textArea.value.toLowerCase();
         return
     }
-    function capitalize(){
-            textAreaArray = textArea.value.split(" ")
-            let result = []
-            for(x of textAreaArray){
-                let upper = x.charAt(0).toUpperCase()
-                let lower = x.slice(1).toLowerCase()
-                let fullWord = upper + lower
-                result.push(fullWord)
-            }
+
+// to capitalize function
+function capitalize(){
+     textAreaArray = textArea.value.split(" ")
+        let result = []
+        for(let x of textAreaArray){
+            let upper = x.charAt(0).toUpperCase()
+            let lower = x.slice(1).toLowerCase()
+            let fullWord = upper + lower
+            result.push(fullWord)
+        }
             textArea.value = result.join(" ")
             return 
             
         }
-        // function deleteAdditionalSpaces(){
-            
-        // }
+
+// delete additional spaces function
+function deleteAdditionalSpaces(){
+    let fullDeletion = []
+    textArea.value = textArea.value.trim()
+    let deleteTextAreaArray = textArea.value.split(" ")
+    for (let y of deleteTextAreaArray){
+        if(y === ""){
+            continue
+        }else{
+            fullDeletion.push(y)
+        }
+
+    }
+    textArea.value = fullDeletion.join(" ")    
+    
+    return
+}
+//word counter function
+function countWords (){
+    let count = 0
+    deleteAdditionalSpaces()
+    let secondTextAreaArray = textArea.value.split(" ")
+    for(z of secondTextAreaArray){
+        if(z === ""){
+            continue
+        }else{
+            count++
+        }
+    }
+    console.log(count);
+    
+    return 
+}
 
 UpperCasing.addEventListener("click", upperCase)
 LowerCasing.addEventListener("click", lowerCase)
 Capitalizing.addEventListener("click", capitalize)
-
+deleteSpaces.addEventListener("click", deleteAdditionalSpaces)
+wordCounter.addEventListener("click", countWords)
 
